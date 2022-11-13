c81a2bad57e0f30742febff44614bcf3ab97e8c1
Carolin Jenner <a.carolinjenner@gmail.com>
2022-11-13 20:35:24 +0100

    feat: adding function to change color, displaying rgb-value while changing color

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-const rangeRed = document.getElementById('range-Red');
-const rangeGreen = document.getElementById('range-Green');
-const rangeBlue = document.getElementById('range-Blue');
+const rangeRed = document.getElementById('range-red');
+const rangeGreen = document.getElementById('range-green');
+const rangeBlue = document.getElementById('range-blue');
+const background = document.getElementById('background');
+const valueDisplay = document.getElementById('value-display');
 
+
+function changeColor() {
+    let red = rangeRed.value;
+    let green = rangeGreen.value;
+    let blue = rangeBlue.value;
+
+console.log(valueDisplay);
+background.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+valueDisplay.innerText = `rgb(${red}, ${green}, ${blue})`;
+
+}
+
+rangeRed.addEventListener("input", changeColor);
+rangeGreen.addEventListener("input", changeColor);
+rangeBlue.addEventListener("input", changeColor);
